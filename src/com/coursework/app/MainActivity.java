8bf5e45aa005a1177949063ff06c6d115d49ce03
Louis Phan <icoding.active@gmail.com>
2016-09-20 00:18:50 +0700

    ABC

---
+++ src/com/coursework/app/MainActivity.java
@@ -0,0 +1,65 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+
+package com.coursework.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.apache.cordova.*;
+
+public class MainActivity extends CordovaActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // Set by <content src="index.html" /> in config.xml
+        //loadUrl(launchUrl);
+        setContentView(R.layout.activity_main);
+        Button btnBirdPage = (Button)this.findViewById(R.id.btnBirdPage);
+        Button btnManageBirdPage = (Button)this.findViewById(R.id.btnManageBirdPage);
+        //Button btnCourseWork3 = (Button)this.findViewById(R.id.btnCourseWork3);
+        btnBirdPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AddBirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnManageBirdPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ListBirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /*btnCourseWork3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ListEventActivity.class);
+                startActivity(intent);
+            }
+        });*/
+    }
+}
